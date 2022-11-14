e672848d1270885c81575416d985a5687f4b4c31
Ogliss <randomdragon2@hotmail.com>
2022-11-14 07:04:00 +0000

    1.4 filtering tweaks

---
+++ 1.4/Source/Patch/Access.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.Steam;
+
+namespace PublisherPlus.Patch
+{
+    internal static class Access
+    {
+        private static readonly MethodInfo Method_Verse_Steam_Workshop_Upload = AccessTools.Method(typeof(Workshop), "Upload", new[] { typeof(WorkshopUploadable) });
+
+        public static void Method_Verse_Steam_Workshop_Upload_Call(WorkshopUploadable item) => Method_Verse_Steam_Workshop_Upload.Invoke(null, new object[] { item });
+    }
+}
